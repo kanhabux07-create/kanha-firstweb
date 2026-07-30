7dd5fa2c51d3dba1c9080184c177e076fb5ea942
kanhabux07-create <kanhabux07@gmail.com>
2026-07-30 21:34:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Welcome to Kanha Study Hub 🚀");
+
+const btn=document.querySelector("button");
+
+btn.onclick=function(){
+
+btn.innerHTML="Let's Learn 🚀";
+
+btn.style.background="green";
+
+}
